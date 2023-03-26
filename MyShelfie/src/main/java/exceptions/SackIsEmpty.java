c807c853a907eed0c5885a542ed5182531793003
Andrea Migliorini <andrea1.migliorini@mail.polimi.it>
2023-03-26 13:21:07 +0200

    SackOfTiles added

---
+++ MyShelfie/src/main/java/exceptions/SackIsEmpty.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class SackIsEmpty extends Exception{
+    /*
+    *
+    * Exception thrown when you try to extract a tile from an empty sack of tiles
+    *
+    * */
+
+    public SackIsEmpty(){
+    }
+}
