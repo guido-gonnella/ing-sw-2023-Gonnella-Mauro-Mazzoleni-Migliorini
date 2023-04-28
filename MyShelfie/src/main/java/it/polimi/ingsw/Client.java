a42c6e3b2918cc3ecf9d0c08c7ba25e4b907c1c0
PierantonioM <126569384+PierantonioM@users.noreply.github.com>
2023-04-28 16:53:52 +0200

    Revert "Socket connection"
    This reverts commit aedf54a69fdbaae71ebf87e93d0f2d049da3bb64.

--- MyShelfie/src/main/java/it/polimi/ingsw/Client.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Client.java
@@ -1,8 +1,6 @@
 package it.polimi.ingsw;
 
-import java.io.*;
-import java.net.Socket;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Client {
     public static void main(String[] args) throws IOException {
@@ -20,45 +18,4 @@ public class Client {
         * */
 
     }
-
-    /**
-     * Method that creates a socket connection with the server
-     *
-     * @param port of the connection
-     * @return the socket
-     * @throws IOException
-     * @author Pierantonio Mauro
-     */
-    public static Socket clientConnection(int port) throws IOException {
-        return new Socket("localhost", port);
-    }
-
-    /**
-     * This method send a object to the server
-     *
-     * @param socket
-     * @param object sent to the server
-     * @throws IOException
-     * @author Pierantonio Mauro
-     */
-    public static void sendData(Socket socket, Object obj) throws IOException {
-        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-        out.writeObject(obj);
-        out.flush();
-    }
-
-    /**
-     * This method receive a object from the server
-     *
-     * @param socket
-     * @return the object received
-     * @throws IOException
-     * @throws ClassNotFoundException
-     * @author Pierantonio Mauro
-     */
-    public static Object recieveData(Socket socket) throws IOException, ClassNotFoundException {
-        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-        return in.readObject();
-    }
-
 }
