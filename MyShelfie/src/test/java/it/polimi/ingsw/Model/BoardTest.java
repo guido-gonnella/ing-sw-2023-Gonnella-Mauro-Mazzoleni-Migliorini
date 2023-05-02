37841aea1fa964fd1aea7ca3ba9da2f23be2952a
Samuele Mazzoleni <samuele1.mazzoleni@mail.polimi.it>
2023-05-02 17:17:57 +0200

    Various changes

--- MyShelfie/src/test/java/it/polimi/ingsw/Model/BoardTest.java
+++ MyShelfie/src/test/java/it/polimi/ingsw/Model/BoardTest.java
@@ -12,12 +12,26 @@ public class BoardTest {
         Board board = new Board();
         SackOfTiles sack = new SackOfTiles();
 
+        final int ROW = 9;
+        final int COL = 9;
+
+        for(int i = 0; i < ROW; i++) {
+            for(int j = 0; j < COL; j++) {
+                board.getGrid()[i][j].setAvailable();
+            }
+        }
+
         try {
             board.fill(sack);
         } catch (SackEmptyException e) {
             System.out.println("Something's wrong...");
         }
 
-
+        for(int i = 0; i < ROW; i++) {
+            for(int j = 0; j < COL; j++) {
+                System.out.println("[" + board.getGrid()[i][j].getTile().get().getType() + "]");
+            }
+            System.out.println("\n");
+        }
     }
 }
