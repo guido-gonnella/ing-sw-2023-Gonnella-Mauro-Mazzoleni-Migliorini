7fd1ac5f989a3ea180202f824939e592bb55deaf
Samuele Mazzoleni <samuele1.mazzoleni@mail.polimi.it>
2023-05-04 19:14:15 +0200

    Some changes

--- MyShelfie/src/main/java/it/polimi/ingsw/Model/Coords.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Model/Coords.java
@@ -1,13 +1,12 @@
 package it.polimi.ingsw.Model;
 
-
 /**
  * Support class for the method adjacent tiles
  * @author Guido Gonnella
  */
-public class coord{
+public class Coords {
     public int x, y;
-    public coord(int a, int b){
+    public Coords(int a, int b){
         this.x = a;
         this.y = b;
     }
