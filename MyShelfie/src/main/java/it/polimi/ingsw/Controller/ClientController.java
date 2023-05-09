697fa3ca06bb6ec61d9ec5be2fc6d45f28d163c3
Andrea Migliorini <andrea1.migliorini@mail.polimi.it>
2023-05-09 16:10:11 +0200

    Clientcontroller mini update BoardTestEmoji experiment

--- MyShelfie/src/main/java/it/polimi/ingsw/Controller/ClientController.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Controller/ClientController.java
@@ -2,13 +2,17 @@ package it.polimi.ingsw.Controller;
 
 import it.polimi.ingsw.Model.Player;
 import it.polimi.ingsw.Model.Coords;
+import it.polimi.ingsw.Model.Space;
+import it.polimi.ingsw.Model.Tile;
 import it.polimi.ingsw.Network.Message.C2S.*;
 import it.polimi.ingsw.Network.Message.Message;
 import it.polimi.ingsw.Network.SocketClient;
 import it.polimi.ingsw.Observer.Observer;
 import it.polimi.ingsw.Observer.ViewObserver;
+import it.polimi.ingsw.View.View;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Controller class for the client
@@ -16,7 +20,9 @@ import java.io.IOException;
  */
 public class ClientController implements Observer, ViewObserver {
 
-    //View view;
+    View view;
+    Optional<Tile>[][] shelf;
+    Space[][] board;
     private SocketClient client;
     private String nick;
 
@@ -38,8 +44,8 @@ public class ClientController implements Observer, ViewObserver {
     public void update(Message msg) {
         switch (msg.getMsgType()){
             //case END_STATS ->
-            //case BOARD_UPDATE ->
-            //case SHELF_UPDATE ->
+            case BOARD_UPDATE -> view.boardshow(board);
+            case SHELF_UPDATE -> view.shelfshow(shelf);
             //case PLAYER_UPDATE ->
             //case HAND_UPDATE ->
         }
