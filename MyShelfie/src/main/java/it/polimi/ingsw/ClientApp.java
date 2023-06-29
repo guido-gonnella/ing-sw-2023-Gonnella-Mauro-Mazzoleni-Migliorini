f7c5e5fff8707212b98b3940992da4d3c8689f2f
Andrea Migliorini <andrea1.migliorini@mail.polimi.it>
2023-06-29 11:25:14 +0200

    ClientApp modified for cmd line inpu

--- MyShelfie/src/main/java/it/polimi/ingsw/ClientApp.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/ClientApp.java
@@ -4,32 +4,20 @@ import it.polimi.ingsw.Controller.NetworkHandler;
 import it.polimi.ingsw.View.Cli;
 import it.polimi.ingsw.View.Gui.JavaFXGui;
 import javafx.application.Application;
-
-import java.util.Objects;
-import java.util.Scanner;
-
 public class ClientApp {
-    //public static it.polimi.ingsw.Network.SocketClient;
 
     public static void main(String[] args) {
 
         boolean cliView;
         boolean socketConnection;
-        Scanner s = new Scanner(System.in);
-        String sel;
 
-        System.out.println("Cli or gui? [c/g]");
-        cliView = !s.next().equals("g");
-        System.out.print(("Want to use socket or RMI connection? [s/r]\n"));
-
-
-        sel = s.next();
-        while (!Objects.equals(sel, "s") && !Objects.equals(sel, "r")){
-            System.out.print("The selection is not valid, please select [s] for socket\n or [r] for RMI\n");
-            sel = s.next();
+        cliView = !args[0].equals("g");
+        socketConnection = !args[1].equals("r");
+        if(socketConnection){
+            System.out.println("Socket Connection Selected");} else {
+            System.out.println("RMI Connection Selected");
         }
 
-        socketConnection = !sel.equals("r");
 
         if(cliView){
             Cli view = new Cli();
@@ -38,6 +26,7 @@ public class ClientApp {
             Thread thread= new Thread(networkHandler, "networkHandler_");
             thread.start();
         }else{
+            System.out.println("Cli Selected");
             Application.launch(JavaFXGui.class);
         }
     }
