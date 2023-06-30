75e18ac5865d80c893287168e4c6d38ca441cb6b
Andrea Migliorini <90915509+Kerby35@users.noreply.github.com>
2023-06-30 11:11:34 +0200

    networkhandlerTaskqueue Renamed networkhandler removed

--- MyShelfie/src/main/java/it/polimi/ingsw/View/Gui/JavaFXGui.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/View/Gui/JavaFXGui.java
@@ -1,14 +1,12 @@
 package it.polimi.ingsw.View.Gui;
-import it.polimi.ingsw.Controller.NetworkHandlerTaskqueue;
+import it.polimi.ingsw.Controller.NetworkHandler;
 import it.polimi.ingsw.View.Gui.SceneControllers.StartSceneController;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
@@ -21,7 +19,7 @@ public class JavaFXGui extends Application {
     @Override
     public void start(Stage stage) {
         Gui2 view = new Gui2();
-        NetworkHandlerTaskqueue networkHandler = new NetworkHandlerTaskqueue(view, true);
+        NetworkHandler networkHandler = new NetworkHandler(view, true);
         view.addObserver(networkHandler);
         Thread thread = new Thread(networkHandler, "networkHandler_");
         thread.start();
